cebf83d74c42024bc018c3115c40030e94384063
John Troy <johntroy@brock-lukers-macbook-pro.local>
2013-10-11 19:46:05 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irrelevote::Application.config.secret_token = '137c2675b1ac9a3ae877b142f106055dc6ede2d89d8f579e40680fbaa6ea01a63cee3c65ef142b38eca03693f60670788e68b0d9a5ba1cabf93581b01743fb50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Irrelevote::Application.config.session_store :cookie_store, key: '_irrelevote_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Irrelevote::Application.config.session_store :active_record_store
